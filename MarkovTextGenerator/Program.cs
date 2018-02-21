0e68a326dd595eced12559f2969eb68e09b3305a
TheKnightOfMagic <sabeen.liaquat.3@gmail.com>
2018-02-21 08:32:09 -0500

    Kind of works
    It works but not very well

--- MarkovTextGenerator/Program.cs
+++ MarkovTextGenerator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace MarkovTextGenerator
 {
+    //
     class Program
     {
         static void Main(string[] args)
@@ -15,29 +16,64 @@ namespace MarkovTextGenerator
             Console.WriteLine("Welcome to Marky Markov's Random Text Generator!");
 
             Console.WriteLine("Enter some text I can learn from (enter single ! to finish): ");
-
-            while (true)
+            bool going = true;
+            while (going == true)
             {
 
-                Console.Write("> ");
+                int counter = 0;
+                string line;
 
-                String line = Console.ReadLine();
-                if (line == "!")
-                    break;
+                // Read the file and display it line by line.  
+                System.IO.StreamReader file = new System.IO.StreamReader("TextFile1.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    //System.Console.WriteLine(line);
 
-                chain.AddString(line);  // Let the chain process this string
+
+
+
+
+
+                    if (line == "!")
+                        going = false;
+
+                    chain.AddString(line);  // Let the chain process this string
+                    counter++;
+                }
+                file.Close();
             }
 
             // Now let's update all the probabilities with the new data
             chain.UpdateProbabilities();
 
             // Okay now for the fun part
-            Console.WriteLine("Done learning!  Now give me a word and I'll tell you what comes next.");
-            Console.Write("> ");
 
-            String word = Console.ReadLine();
-            String nextWord = chain.GetNextWord(word);
-            Console.WriteLine("I predict the next word will be " + nextWord);
+            String word = chain.GetRandomStartingWord();
+            while (true)
+            {
+
+
+                //Console.WriteLine("Done learning!  Now give me a word and I'll tell you what comes next.");
+                //Console.Write("> ");
+
+                
+                String nextWord = chain.GetNextWord(word);
+                   Console.Write(" "+ nextWord);
+                if (nextWord.Contains(".") || nextWord.Contains("!") || nextWord.Contains("?"))
+                {
+                    Console.WriteLine("\n");
+                    word = chain.GetRandomStartingWord();
+                }
+                else
+                {
+                    word = nextWord;
+                }
+                if (word == "peckish")
+                {
+                    break;
+                }
+               
+            }
         }
     }
 }
